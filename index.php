c8567b4d8757747d68aaa4e5a217df1a06564d50
mouhcine <mouhcine15.00@gmail.com>
2022-06-28 14:58:21 +0200

    array

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php 
+    $faqs = [
+        [
+            'question' => 'domanda numero 1',
+            'answer' => 'rsposta numero 2'
+        ],
+        [
+            'question' => 'domanda numero 2',
+            'answer' => 'rsposta numero 2'
+        ],
+        [
+            'question' => 'domanda numero 3',
+            'answer' => 'rsposta numero 3'
+        ],
+        [
+            'question' => 'domanda numero 4',
+            'answer' => 'rsposta numero 4'
+        ],
+
+    ]
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
